a5a72161443ae24cf8694886fe7ab70fa5374106
karolmetzker <110042835+karolmetzker@users.noreply.github.com>
2023-06-20 19:00:29 -0300

    Update visao_empresa.py

--- pages/visao_empresa.py
+++ pages/visao_empresa.py
@@ -74,8 +74,8 @@ traffic_options = st.sidebar.multiselect(
     ('Baixo', 'Médio','Alto','Congestionamento'))
 st.sidebar.markdown('''---''')
 
-linhas_selecionadas = df['Order_Date'] < date_slider #filtrar do inicio até essa data
-df= df.loc[linhas_selecionadas, :]
+#linhas_selecionadas = df['Order_Date'] < date_slider #filtrar do inicio até essa data
+#df= df.loc[linhas_selecionadas, :]
 #st.dataframe ( df )
 
 linhas_selecionadas =  df['Road_traffic_density'].isin( traffic_options )
